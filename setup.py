bbb734e68ad12be9aec820be8ee25d3785769d40
Jorge Niedbalski <jnr@metaklass.org>
2015-04-06 21:30:11 -0300

    Added travis support
    Signed-off-by: Jorge Niedbalski <jnr@metaklass.org>

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup
 setup(
     author="Jorge Niedbalski R. <jnr@metaklass.org>",
     name="ssh-exec-test",
+    version="0.1.1",
     setup_requires=['pbr'],
     pbr=True,
 )
